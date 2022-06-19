a8737a24820f3197855a7441460757e57aa0bbec
Idiatou Diallo <d.idiatou@gmail.com>
2022-06-19 18:20:04 +0200

    Set and comment

--- code1.py
+++ code1.py
@@ -1,3 +1,4 @@
+# Liste
 mylist = ["bana", "cheri", "apple"]
 print(mylist)
 
@@ -33,7 +34,7 @@ print(a)
 b= [i*i for i in a]
 print(b)
 
-
+#Tuple
 mytuple = ("Max", 28,"Boston")
 print(mytuple)
 print(type(mytuple))
@@ -54,7 +55,7 @@ tuple=('e', 'e', 'd','f','r','r','r',2)
 print(tuple.count('r'))
 print(tuple.index('f')) 
 
-
+#Dictionary
 mydict = {"name": "Max", "age": 32, "city":"Paris"}
 print(mydict)
 mydict["mail"] =  "arri@gmail.com"
@@ -69,5 +70,27 @@ print(mydict)
 print(mydict.popitem())
 
 for key, value in mydict.items():
-    print(key,value)
+    print(key,value) 
 
+# Sets
+x = set()
+x.add(1)
+x.add(2)
+x.add(3)
+print(x)
+x.discard(3)
+print(x)
+for i in x:
+    print(i)
+
+if 5 in x:
+    print("yes")
+else:
+    print("no")
+
+pds={1,3,5,7,9}
+evens = {0,2,4,6,8}
+primes= {2,3,5,7}
+print(pds.union(evens))
+print(primes.intersection(pds))
+print(evens.symmetric_difference(primes))
